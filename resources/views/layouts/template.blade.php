301464097cd5ee88350109b4b55d5786eac2b026
jrochaandrade <jrochaandrade@gmail.com>
2023-04-24 00:44:30 -0400

    CSS com erro

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,7 +3,7 @@
     <head>        
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">        
+        <!-- <link href="css/style.css" rel="stylesheet"> -->        
         <title>@yield('title')</title>
         <!-- <script src="https://cdn.tailwindcss.com"></script> --> <!-- Esta dando erro essa linha -->       
              
